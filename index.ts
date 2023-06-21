47d7d34f0ba492b5b03f035e019f49cfc90625d8
tea551234 <tea551234@gmail.com>
2023-06-22 01:16:03 +0800

    2023 06 22 01:15 st typescript interface obj

--- index.ts
+++ index.ts
@@ -1,4 +1,42 @@
-function a(...args: any[]) {
-    console.log(arguments)
+// function a(...args: any[]) {
+//     console.log(arguments)
+// }
+// a(1, 2)
+
+
+// function add(a: number, b: number) {
+//     return a + b;
+// }
+
+
+// console.log(add(1, 2));
+
+//定義對象
+
+
+interface User {
+    name: string
+    age: number
+    email: string
 }
-a(1, 2)
+
+
+// function add(user: User) {
+//     return '姓名:' + user.name + ' '+'年紀' + user.age + ' '+'信箱' + user.email;
+// }
+// console.log(add({name: "John", age:12, email: "John@example"}));
+
+interface Obj {
+    ueerid: number[],
+    add: (this: Obj, num: number) =>void
+}
+
+
+let obj: Obj = {
+    ueerid: [1, 2, 3],
+    add(this: Obj, num: number) {
+        this.ueerid.push(num);
+    }
+}
+
+obj.add(10);
